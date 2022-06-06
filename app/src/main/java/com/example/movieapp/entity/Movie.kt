787e60b6b4d7d88ac624d5b9fb076406c0d0b809
emirk <emir.karabey1@hotmail.com>
2022-06-06 13:29:40 +0300

    Add navigation graph and model

---
+++ app/src/main/java/com/example/movieapp/entity/Movie.kt
@@ -0,0 +1,14 @@
+package com.example.movieapp.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity(tableName = "movies")
+data class Movie(@PrimaryKey(autoGenerate = true)
+                   @ColumnInfo(name = "movie_id") @NotNull var movie_id:Int,
+                 @ColumnInfo(name = "movie_name") @NotNull var movie_name:String,
+                 @ColumnInfo(name = "movie_date") @NotNull var movie_date:String,
+                 @ColumnInfo(name = "movie_img") @NotNull var movie_img:String) {
+}

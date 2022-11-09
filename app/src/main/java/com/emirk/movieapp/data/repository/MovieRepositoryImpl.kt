0dc9b4f76dae5f2898a8ef41771d633b0f97545f
emirk <emir.karabey1@hotmail.com>
2022-11-09 14:33:20 +0300

    Add watch later movie function and get watch later movie function in repository

--- app/src/main/java/com/emirk/movieapp/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/emirk/movieapp/data/repository/MovieRepositoryImpl.kt
@@ -4,19 +4,22 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.emirk.movieapp.data.local.WatchLaterDao
+import com.emirk.movieapp.data.local.entity.MovieEntity
 import com.emirk.movieapp.data.paged.*
 import com.emirk.movieapp.data.remote.ApiService
 import com.emirk.movieapp.data.remote.model.movie_details.MovieDetails
-import com.emirk.movieapp.data.remote.model.movie_lists.Movie
 import com.emirk.movieapp.domain.repository.MovieRepository
+import com.emirk.movieapp.ui.model.MovieUiModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class MovieRepositoryImpl @Inject constructor(
-    private val apiService: ApiService
+    private val apiService: ApiService,
+    private val watchLaterDao: WatchLaterDao
 ) : MovieRepository {
-    override fun getPopularMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>> {
+    override fun getPopularMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>> {
         return Pager(
             config = PagingConfig(
                 pageSize = NETWORK_PAGE_SIZE
@@ -25,7 +28,7 @@ class MovieRepositoryImpl @Inject constructor(
         ).flow.cachedIn(coroutineScope)
     }
 
-    override fun getLatestMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>> {
+    override fun getLatestMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>> {
         return Pager(
             config = PagingConfig(
                 pageSize = NETWORK_PAGE_SIZE
@@ -34,7 +37,7 @@ class MovieRepositoryImpl @Inject constructor(
         ).flow.cachedIn(coroutineScope)
     }
 
-    override fun getTopRatedMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>> {
+    override fun getTopRatedMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>> {
         return Pager(
             config = PagingConfig(
                 pageSize = NETWORK_PAGE_SIZE
@@ -43,7 +46,7 @@ class MovieRepositoryImpl @Inject constructor(
         ).flow.cachedIn(coroutineScope)
     }
 
-    override fun getUpComingMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>> {
+    override fun getUpComingMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>> {
         return Pager(
             config = PagingConfig(
                 pageSize = NETWORK_PAGE_SIZE
@@ -52,7 +55,7 @@ class MovieRepositoryImpl @Inject constructor(
         ).flow.cachedIn(coroutineScope)
     }
 
-    override fun getNowPlayingMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>> {
+    override fun getNowPlayingMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>> {
         return Pager(
             config = PagingConfig(
                 pageSize = NETWORK_PAGE_SIZE
@@ -63,6 +66,13 @@ class MovieRepositoryImpl @Inject constructor(
 
     override suspend fun getMovieDetailById(id: Int): MovieDetails {
         return apiService.getMovieDetailById(id)
+    }
+
+    override suspend fun addWatchLaterMovie(movieEntity: MovieEntity) =
+        watchLaterDao.addWatchLaterMovie(movieEntity)
+
+    override fun getWatchLaterMovie(): List<MovieEntity> {
+        return watchLaterDao.getAllFavoriteMovie()
     }
 
     companion object {
--- app/src/main/java/com/emirk/movieapp/domain/repository/MovieRepository.kt
+++ app/src/main/java/com/emirk/movieapp/domain/repository/MovieRepository.kt
@@ -1,16 +1,20 @@
 package com.emirk.movieapp.domain.repository
 
 import androidx.paging.PagingData
+import com.emirk.movieapp.data.local.entity.MovieEntity
 import com.emirk.movieapp.data.remote.model.movie_details.MovieDetails
-import com.emirk.movieapp.data.remote.model.movie_lists.Movie
+import com.emirk.movieapp.ui.model.MovieUiModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 
 interface MovieRepository {
-    fun getPopularMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>>
-    fun getLatestMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>>
-    fun getTopRatedMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>>
-    fun getUpComingMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>>
-    fun getNowPlayingMovies(coroutineScope: CoroutineScope): Flow<PagingData<Movie>>
+    fun getPopularMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>>
+    fun getLatestMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>>
+    fun getTopRatedMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>>
+    fun getUpComingMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>>
+    fun getNowPlayingMovies(coroutineScope: CoroutineScope): Flow<PagingData<MovieUiModel>>
     suspend fun getMovieDetailById(id: Int): MovieDetails
+
+    suspend fun addWatchLaterMovie(movieEntity: MovieEntity)
+    fun getWatchLaterMovie(): List<MovieEntity>
 }

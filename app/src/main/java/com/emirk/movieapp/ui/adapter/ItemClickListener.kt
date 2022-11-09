9befdaaf88955e9d1092bd474d34e2504a6dca38
emirk <emir.karabey1@hotmail.com>
2022-11-09 14:50:08 +0300

    Change item click listener. Add watch later click function

--- app/src/main/java/com/emirk/movieapp/ui/adapter/ItemClickListener.kt
+++ app/src/main/java/com/emirk/movieapp/ui/adapter/ItemClickListener.kt
@@ -1,5 +1,8 @@
 package com.emirk.movieapp.ui.adapter
 
+import com.emirk.movieapp.ui.model.MovieUiModel
+
 interface ItemClickListener {
     fun onClickMovie(id: Int)
+    fun onClickWatchLaterButton(movieUi: MovieUiModel)
 }

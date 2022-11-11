c9155247916e2c0779a637788803107993486c0c
emirk <emir.karabey1@hotmail.com>
2022-11-11 13:07:13 +0300

    Change main activity location

--- app/src/main/java/com/emirk/movieapp/ui/MainActivity.kt
+++ app/src/main/java/com/emirk/movieapp/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.emirk.movieapp
+package com.emirk.movieapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.emirk.movieapp.R
 import com.emirk.movieapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
